6aaf8e0e7e7f26b55281ddc9fb07a4d61f9fa2a7
DannyloC123 <DannyloCorreia2006@gmail.com>
2025-12-11 20:58:19 -0500

    Was able to make the player move and shoot at the same time, now working on the mechanicls still because it now glides in either direction but doesn't stop when no keys are being pressed

--- FinalProject/SpaceInvaders.c
+++ FinalProject/SpaceInvaders.c
@@ -7,43 +7,28 @@
 #include <unistd.h>
 #include "gfx.h"
 
-// -----------------------------
-// CONSTANTS
-// -----------------------------
 #define WIDTH 800
 #define HEIGHT 900
 #define INVADER_ROWS 4
 #define INVADER_COLS 8
+#define MAX_BULLET 10
+#define PLAYER_SPEED 10
 
-// -----------------------------
-// STRUCTS
-// -----------------------------
 typedef struct {
     int x, y;
     int width, height;
 } Player;
 
 typedef struct {
-    int x, y;
-    int alive;
+    int x, y, alive;
 } Invader;
 
 typedef struct {
     int x, y;
-    int width;
-    int height;
+    int width, height;
     int active;
 } Bullet;
 
-typedef struct {
-    int leftHeld;
-    int rightHeld;
-    int shootPressed;
-} Input;
-
-// -----------------------------
-// INITIALIZATION
-// -----------------------------
 Player initPlayer() {
     Player p;
     p.x = WIDTH / 2;
@@ -54,89 +39,56 @@ Player initPlayer() {
 }
 
 Bullet initBullet() {
-    Bullet b;
-    b.x = 0;
-    b.y = 0;
-    b.active = 0;
-    b.width = 10;
-    b.height = 5;
+    Bullet b = {0, 0, 10, 5, 0};
     return b;
 }
 
-Input initInput() {
-    Input in;
-    in.leftHeld = 0;
-    in.rightHeld = 0;
-    in.shootPressed = 0;
-    return in;
-}
-
 void initInvaders(Invader inv[INVADER_ROWS][INVADER_COLS]) {
-    for (int r = 0; r < INVADER_ROWS; r++) {
-        for (int c = 0; c < INVADER_COLS; c++) {
-            inv[r][c].x = 70 + c * 90;
-            inv[r][c].y = 50 + r * 75;
-            inv[r][c].alive = 1;
-        }
-    }
+    for (int r = 0; r < INVADER_ROWS; r++)
+        for (int c = 0; c < INVADER_COLS; c++)
+            inv[r][c] = (Invader){70 + c*90, 50 + r*75, 1};
 }
 
-// -----------------------------
-// DRAWING HELPERS
-// -----------------------------
 void gfx_fill_rect(int x, int y, int w, int h) {
-    for (int i = 0; i < h; i++) {
+    for (int i = 0; i < h; i++)
         gfx_line(x, y + i, x + w, y + i);
-    }
 }
 
 void drawPlayer(Player p) {
-    gfx_color(0, 255, 0);
+    gfx_color(0,255,0);
     gfx_fill_rect(p.x, p.y, p.width, p.height);
 }
 
 void drawInvaders(Invader inv[INVADER_ROWS][INVADER_COLS]) {
-    gfx_color(255, 0, 0);
-    for (int r = 0; r < INVADER_ROWS; r++) {
-        for (int c = 0; c < INVADER_COLS; c++) {
-            if (inv[r][c].alive) {
+    gfx_color(255,0,0);
+    for (int r = 0; r < INVADER_ROWS; r++)
+        for (int c = 0; c < INVADER_COLS; c++)
+            if (inv[r][c].alive)
                 gfx_fill_rect(inv[r][c].x, inv[r][c].y, 50, 50);
-            }
-        }
-    }
 }
 
 void drawBullet(Bullet b) {
     if (!b.active) return;
-    gfx_color(255, 255, 0);
+    gfx_color(255,255,0);
     gfx_fill_rect(b.x, b.y, 4, 10);
 }
 
-// -----------------------------
-// GAME LOGIC
-// -----------------------------
 void shoot(Bullet *b, Player *p) {
     b->active = 1;
-    b->x = p->x + p->width / 2;
+    b->x = p->x + p->width/2 - 2;
     b->y = p->y;
 }
 
 void updateBullet(Bullet *b) {
     if (!b->active) return;
-
     b->y -= 10;
-    if (b->y < 0)
-        b->active = 0;
+    if (b->y < 0) b->active = 0;
 }
 
-// -----------------------------
-// MAIN GAME LOOP
-// -----------------------------
 int main() {
     Player player = initPlayer();
-    Input in = initInput();
-    Bullet bullets[3];
-    for (int i = 0; i < 3; i++)
+    Bullet bullets[MAX_BULLET];
+    for (int i = 0; i < MAX_BULLET; i++)
         bullets[i] = initBullet();
 
     Invader invaders[INVADER_ROWS][INVADER_COLS];
@@ -145,52 +97,62 @@ int main() {
     gfx_open(WIDTH, HEIGHT, "Space Invaders");
 
     int Running = 1;
+    int keyLeft = 0;
+    int keyRight = 0;
+    int lastSpaceState = 0;
 
     while (Running) {
         gfx_clear();
 
         drawPlayer(player);
         drawInvaders(invaders);
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < MAX_BULLET; i++)
             drawBullet(bullets[i]);
 
         gfx_flush();
-        usleep(16000); // 60 FPS
-
-
-        in.leftHeld = 0;
-        in.rightHeld = 0;
 
 
         while (gfx_event_waiting()) {
-            char key = gfx_wait();
-
-            if (key == 'q') Running = 0;
-            if (key == 'a') in.leftHeld = 1;
-            if (key == 'd') in.rightHeld = 1;
-            if (key == ' ') in.shootPressed = 1;
-        }
-
-
-        if (in.leftHeld && player.x > 10)
-            player.x -= 10;
-
-        if (in.rightHeld && player.x + player.width + 15 < WIDTH)
-            player.x += 10;
-
-
-        if (in.shootPressed) {
-            for (int i = 0; i < 3; i++) {
-                if (!bullets[i].active) {
-                    shoot(&bullets[i], &player);
-                    break;
+            char c = gfx_wait();
+                        
+            if (c == 'q') {
+                Running = 0;
+            }
+            
+            if (c == 'a') {
+                keyLeft = 1;
+                keyRight = 0;
+            }
+            else if (c == 'd') {
+                keyRight = 1;
+                keyLeft = 0;
+            }
+            else if (c == ' ') {
+                for (int i = 0; i < MAX_BULLET; i++) {
+                    if (!bullets[i].active) {
+                        shoot(&bullets[i], &player);
+                        break;
+                    }
                 }
             }
-            in.shootPressed = 0;
+            else if ((unsigned char)c > 127) {
+                char released = c - 128;
+                if (released == 'a') keyLeft = 0;
+                if (released == 'd') keyRight = 0;
+            }
         }
 
-        for (int i = 0; i < 3; i++)
+        if (keyLeft && player.x > 10) {
+            player.x -= PLAYER_SPEED;
+        }
+        if (keyRight && player.x + player.width + 15 < WIDTH) {
+            player.x += PLAYER_SPEED;
+        }
+
+        for (int i = 0; i < MAX_BULLET; i++)
             updateBullet(&bullets[i]);
+
+        usleep(16000);  // ~60 FPS
     }
 
     return 0;
